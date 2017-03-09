2a936fbbc7433d073117b23114fd4c8134387e56
Miguel Korn <20448@ma-web.nl>
2017-03-09 13:54:59 +0100

    changed auth

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -10,14 +10,10 @@ class testController extends Controller
     //
     public function show()
     {
-<<<<<<< HEAD
         $user = DB::table('users')->get();
 
         print '<pre>';
         print_r($user);
         print '</pre>';
-=======
-        $user = DB::select("");
->>>>>>> origin/master
     }
 }
